563ec3cc9cd09364f99a8afe7278cb5a3e6a9431
brainysoon <brainysoon@gmail.com>
2018-05-13 10:09:55 +0800

    [ Jenkins ] - Try to deploy to another machine docker instead of current docker.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,25 @@
 cp build/libs/2018-04-02-01-27-37-1522632457.jar docker/app.jar
 
-docker rmi -f tw-camp-jenkins
+tar -czvf tw-camp-jenkins.tar.gz docker/
+
+scp -i ./stage_icusin_ubuntu ./tw-camp-jenkins.tar.gz root@stage.icusin.com:/home/
+
+ssh -i ./stage_icusin_ubuntu -o StrictHostKeyChecking=no root@stage.icusin.com << stage-icusin-remote
+
+cd /home
+
+rm -r -f docker
+
+tar -zxvf tw-camp-jenkins.tar.gz
 
 cd docker
 
 docker build -t tw-camp-jenkins .
 
-docker run -p 4000:4000  tw-camp-jenkins
+docker rm -f tw-camp
+
+docker run -d -p 4000:4000 --name='tw-camp' tw-camp-jenkins
+
+stage-icusin-remote
+
+echo finish deploy
